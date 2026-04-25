31dee4cfc3dc885c84adce994f219e1595bd739c
Mehboob Ali <mehboobali@Mehboobs-MacBook-Air.local>
2026-04-25 23:01:16 +0500

    condition statment

---
+++ condition /condition.js
@@ -0,0 +1,42 @@
+//if satement 
+let age ;
+age = 20;
+if (age > 18){
+    console.log("you can vote")
+}
+    
+   //if else statment 
+   let umar ;
+   umar=18;
+   if(umar >20){
+    console.log("you can vote ")
+   }
+   else{
+    console.log("you can't vote ")
+   }
+
+//else if statment 
+let color ;
+color = "white" ;
+if (color === "red"){
+    console.log("the color is red")
+
+}
+else if (color == "blue"){
+    console.log("the color is blue")
+
+}
+else if(color === "purple "){
+    console.log("the color is purple")
+}
+else {
+    console.log("color is not match")
+}
+//ternary operator 
+let ali ;
+ali =25;
+let result = ali>=20? "adult ": "underage";
+console.log(result);
+
+    
+
---
+++ gradding /gradding.js
@@ -0,0 +1,23 @@
+let score;
+score=50;
+if(score >=90&& score <=100){
+   
+    console.log( "you are in grad A")
+}
+else if(score>=70 && score <=89){
+    
+console.log("you are in B grad ")
+}
+else if(score>=60 && score <=69){
+   
+console.log("you are in C grad ")
+}
+else if(score>=50 && score <=59){
+    
+console.log("you are in D grad ")
+}
+else(score>=0 && score <=49)
+{
+   
+    console.log("you are fail ")
+}
