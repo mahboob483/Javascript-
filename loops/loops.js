b02d63f4a474b3abb8afd066b3bc6586b671eb83
Mehboob Ali <mehboobali@Mehboobs-MacBook-Air.local>
2026-04-28 01:58:27 +0500

    for in and of loof

---
+++ loops/loops.js
@@ -0,0 +1,45 @@
+
+//for(let i=0; i<=5 ;i++){
+//console.log("i = ");
+//}
+
+
+//sum 1 to 5 using for loop 
+/*let sum = 0 ;
+for (let i = 1 ; i <= 5 ; i++){
+    sum = sum + i;
+}
+console.log("sum = ", sum );
+console.log("loop is ended ");
+  */  
+  /*for of loop 
+ let size=0;
+ let str = "javascript ";
+ for (let i of str ){
+    console.log("i=",i)
+    size++;
+ }
+console.log("string size =",size)
+*/
+//for in loop 
+/*let student = {
+    name: "javascript",
+    age: 20,
+    cgpa: 3.5,
+    ispass: true
+};
+
+for (let key in student) {
+    console.log("key =", key, "value =", student[key]);
+}*/
+/*for (let i =0 ;i <= 100; i++){
+    
+    if (i % 2==0){
+        console.log("i=",i)
+    }else{
+
+    }
+}
+
+
+
